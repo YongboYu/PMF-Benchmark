095c27a5ac27fd9223a1b161eda8c57aecd33984
YongboYu <0330yuyongbo@gmail.com>
2025-04-27 15:29:56 +0200

    refact: Refactor regression model training with horizon-specific lags
    Introduced support for horizon-specific lag configurations in YAML and codebase. Enhanced integration with WandB for logging study visualizations and training metrics. Improved error handling, added timing for training phases, and streamlined model training workflows.

--- models/regression_models.py
+++ models/regression_models.py
@@ -13,6 +13,10 @@ from darts.models import (
     XGBModel,
     LightGBMModel
 )
+from optuna.integration.wandb import WeightsAndBiasesCallback
+from optuna.visualization import plot_optimization_history, plot_param_importances, plot_parallel_coordinate
+
+
 from utils.optuna_manager import OptunaManager
 from darts.metrics import mae
 from utils.wandb_logger import WandbLogger
@@ -82,10 +86,14 @@ class RegressionModels:
         params = {}
         param_ranges = self.models[model_name]["params"]
         
+        # Get horizon-specific lags configuration
+        horizon_key = f'horizon_{horizon}'
+        lags_config = self.config['common']['lags'][horizon_key]
+        
         # Get lags as a hyperparameter to optimize
-        min_lag = self.config['common']['lags']['min_lag']
-        max_lag = self.config['common']['lags']['max_lag']
-        step_size = self.config['common']['lags']['step_size']
+        min_lag = lags_config['min_lag']
+        max_lag = lags_config['max_lag']
+        step_size = lags_config['step_size']
         
         # Create list of possible lag values
         possible_lags = list(range(min_lag, max_lag + 1, step_size))
@@ -136,14 +144,15 @@ class RegressionModels:
             for pred, true in zip(val_predictions, val_output_seq):
                 val_scores.append(mae(true, pred))
             val_score = np.mean(val_scores)
-            
+
+            wandb.log({'val_mae': val_score})
             # Log metrics and parameters
-            if self.wandb_logger is not None:
-                self.wandb_logger.log_trial_metrics(
-                    metrics={'mae': val_score},
-                    params=params,
-                    model_name=model_name
-                )
+            # if self.wandb_logger is not None:
+                # self.wandb_logger.log_trial_metrics(
+                #     metrics={'mae': val_score},
+                #     params=params,
+                #     model_name=model_name
+                # )
             
             return val_score
             
@@ -152,117 +161,161 @@ class RegressionModels:
             raise optuna.exceptions.TrialPruned()
 
     def train_and_predict(self, model_name: str, train, val, test, transformer,
-                         horizon: int, dataset: str, study: Optional[optuna.Study] = None,
-                         wandb_logger: WandbLogger = None) -> Dict[str, Any]:
+                          horizon: int, dataset: str, study: Optional[optuna.Study] = None,
+                          wandb_logger: WandbLogger = None) -> Dict[str, Any]:
         """Train model and generate predictions using seq2seq approach"""
-        # Set wandb_logger as instance attribute
-        self.wandb_logger = wandb_logger
-
-        if not self.models[model_name]["enabled"]:
-            logger.info(f"Model {model_name} is disabled in config")
-            return {}
-
-        wandb.log({f"regression_training_model": model_name})
-        best_params = {}
-
         try:
-            # Create study using OptunaManager if not provided
-            if study is None:
-                study = self.optuna_manager.create_study(
-                    dataset=dataset,
-                    horizon=horizon,
-                    model_group='regression',
-                    model_name=model_name
-                )
-            
-            # Get wandb callback if logger is provided
-            wandb_callback = None
+            # Set wandb_logger as instance attribute
+            self.wandb_logger = wandb_logger
+
+            if not self.models[model_name]["enabled"]:
+                logger.info(f"Model {model_name} is disabled in config")
+                return {}
+
+            # Start timing the entire training process
+            training_start_time = time.time()
+
+            # Get wandb initialization settings from config
+            wandb_kwargs = self.config.get('wandb', {}).copy()
+            # Update run name to include model specific information
+            # wandb_kwargs['name'] = f"{wandb_kwargs.get('name', '')}_{model_name}_{dataset}_h{horizon}"
+
+            # Add wandb config logging here
             if wandb_logger is not None:
-                wandb_callback = wandb_logger.log_optuna_study(study, model_name, dataset, horizon)
+                # with wandb.init(**wandb_kwargs):
+                    wandb.config.update({
+                        "model_params": {
+                            model_name: self.models[model_name]["params"]
+                        },
+                        "common_params": {
+                            "lags": self.config['common']['lags'],
+                            "n_trials": self.config['common']['n_trials']
+                        }
+                    })
 
-            # Run optimization with callback if available
-            optimize_args = {
-                'func': lambda trial: self.objective(
-                    trial,
-                    model_name,
-                    self.models[model_name]["model"],
-                    train,
-                    val,
-                    horizon
-                ),
-                'n_trials': self.config['common']['n_trials'],
-                'catch': (Exception,)
-            }
-            
-            if wandb_logger is not None:
-                optimize_args['callbacks'] = [wandb_callback]
-            
-            study.optimize(**optimize_args)
+                    # Create study and run optimization
+                    if study is None:
+                        study = self.optuna_manager.create_study(
+                            dataset=dataset,
+                            horizon=horizon,
+                            model_group='regression',
+                            model_name=model_name
+                        )
 
-            # Log study results after optimization
-            if wandb_logger is not None:
-                wandb_logger.log_study_results(study, model_name, dataset, horizon)
+                    wandb_callback = WeightsAndBiasesCallback(
+                        metric_name='val_mae',
+                        wandb_kwargs=wandb_kwargs
+                    )
+                    # # Get wandb callback if logger is provided
+                    # wandb_callback = wandb_logger.log_optuna_study(
+                    #     study,
+                    #     model_name,
+                    #     dataset,
+                    #     horizon,
+                    #     wandb_kwargs=wandb_kwargs
+                    # )
 
-            # Get the best parameters if available
-            if study.best_trial is not None:
-                best_params = study.best_params
-                best_params['output_chunk_length'] = horizon
-                
-                if 'lags' not in best_params:
-                    best_params['lags'] = study.best_trial.params.get('lags')
+                    # Run optimization with callback
+                    optimize_args = {
+                        'func': lambda trial: self.objective(
+                            trial,
+                            model_name,
+                            self.models[model_name]["model"],
+                            train,
+                            val,
+                            horizon
+                        ),
+                        'n_trials': self.config['common']['n_trials'],
+                        'catch': (Exception,),
+                        'callbacks': [wandb_callback] if wandb_callback else None
+                    }
 
-                # Train final model on combined data
-                combined_train = train.concatenate(val) if val is not None else train
-                model = self.models[model_name]["model"](**best_params)
-                model.fit(combined_train)
+                    study.optimize(**optimize_args)
 
-                # Create seq2seq test dataset for final evaluation
-                test_input_seq, test_output_seq = self.data_loader.create_seq2seq_io_data(
-                    train=train, val=val, test=test, 
-                    input_length=best_params['lags'], 
-                    output_length=horizon, 
-                    dataset_type='test'
-                )
-                
-                # Generate final predictions
-                all_predictions = []
-                for input_seq in test_input_seq:
-                    pred = model.predict(n=horizon, series=input_seq)
-                    all_predictions.append(pred)
+                    # Log study results after optimization
+                    # if wandb_logger is not None:
+                    #     wandb_logger.log_study_results(study, model_name, dataset, horizon)
+                    wandb.log({"optimization_history": plot_optimization_history(study)})
+                    wandb.log({"param_importances": plot_param_importances(study)})
+                    wandb.log({"parallel_coordinate": plot_parallel_coordinate(study)})
 
-                # Log final results
-                wandb.log({
-                    f"regression_{model_name}_best_params": best_params,
-                    f"regression_{model_name}_trained": True,
-                    f"regression_{model_name}_n_trials": len(study.trials),
-                    f"regression_{model_name}_best_value": study.best_value
-                })
+                    # Get the best parameters if available
+                    if study.best_trial is not None:
+                        best_params = study.best_params
+                        best_params['output_chunk_length'] = horizon
 
-                return {
-                    'predictions': all_predictions,
-                    'actuals': test_output_seq,
-                    'model': model,
-                    'best_params': best_params,
-                    'study': study
-                }
-            
-            # Return empty results if no successful trials
-            return {
-                'seq_predictions': None,
-                'seq_actuals': None,
-                'ground_truth': None,
-                'model': None,
-                'best_params': {},
-                'study': study,
-            }
+                        if 'lags' not in best_params:
+                            best_params['lags'] = study.best_trial.params.get('lags')
+
+                        # Train final model on combined data
+                        combined_train = train.concatenate(val) if val is not None else train
+                        model = self.models[model_name]["model"](**best_params)
+                        
+                        # Time the model fitting specifically
+                        fit_start_time = time.time()
+                        model.fit(combined_train)
+                        fit_time = time.time() - fit_start_time
+                        
+                        # Calculate total training time (including optimization)
+                        total_training_time = time.time() - training_start_time
+                        wandb.log({"training_time": total_training_time})
+
+                        # Create seq2seq test dataset for final evaluation
+                        test_input_seq, test_output_seq = self.data_loader.create_seq2seq_io_data(
+                            train=train, val=val, test=test,
+                            input_length=best_params['lags'],
+                            output_length=horizon,
+                            dataset_type='test'
+                        )
+
+                        # Generate final predictions
+                        all_predictions = []
+                        for input_seq in test_input_seq:
+                            pred = model.predict(n=horizon, series=input_seq)
+                            all_predictions.append(pred)
+
+                        # Log final results
+                        wandb.run.summary.update({
+                            "best_params": best_params,
+                            "best_val_value": study.best_value
+                        })
+                        # wandb.log({
+                        #     f"regression_{model_name}_best_params": best_params,
+                        #     f"regression_{model_name}_trained": True,
+                        #     f"regression_{model_name}_n_trials": len(study.trials),
+                        #     f"regression_{model_name}_best_value": study.best_value,
+                        #     f"regression_{model_name}_fit_time": fit_time,
+                        #     f"regression_{model_name}_total_training_time": total_training_time
+                        # })
+
+                        return {
+                            'predictions': all_predictions,
+                            'actuals': test_output_seq,
+                            'model': model,
+                            'best_params': best_params,
+                            'study': study,
+                            # 'fit_time': fit_time,
+                            'training_time': total_training_time
+                        }
+
+                    # Return empty results if no successful trials
+                    return {
+                        'predictions': None,
+                        'actuals': None,
+                        'ground_truth': None,
+                        'model': None,
+                        'best_params': {},
+                        'study': study,
+                    }
 
         except Exception as e:
             error_msg = f"Error training {model_name}: {str(e)}"
             logger.error(error_msg)
-            wandb.log({
-                f"regression_{model_name}_error": error_msg,
-                f"regression_{model_name}_failed": True
-            })
+            # if wandb_logger is not None:
+            #     wandb.log({
+            #         f"regression_{model_name}_error": error_msg,
+            #         f"regression_{model_name}_failed": True
+            #     })
             raise
 
     def get_model_names(self) -> list:
