68887d58072279b67995f4fa2a5b6e53bc50e19c
YongboYu <0330yuyongbo@gmail.com>
2025-06-30 00:03:47 +0200

    refact: remove outdated tests

--- tests/setup_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create and activate environment
-conda create -n pmf-benchmark python=3.11 -y
-source activate pmf-benchmark
-
-# Install remaining packages via pip
-pip install -r requirements.txt
